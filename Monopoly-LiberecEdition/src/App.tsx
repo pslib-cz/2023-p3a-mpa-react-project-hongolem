b3d71c9ceffb58e2511fbf93baa6c2f72ceec460
Jan Holý <hongolem@seznam.cz>
2024-05-13 15:45:00 +0200

    contrast, bg, browserRouter

--- Monopoly-LiberecEdition/src/App.tsx
+++ Monopoly-LiberecEdition/src/App.tsx
@@ -1,17 +1,17 @@
-import {RouterProvider, createHashRouter, createRoutesFromElements, Route} from "react-router-dom";
+import {RouterProvider, createRoutesFromElements, Route, createBrowserRouter} from "react-router-dom";
 import Game from "./pages/Game.tsx";
 import Menu from "./pages/Menu.tsx";
 import Winner from "./pages/Winner.tsx";
 
 export const App = () => {
-    const router = createHashRouter(
+    const router = createBrowserRouter(
         createRoutesFromElements(
             <>
                 <Route path="/" element={<Menu />} />
                 <Route path="/Game" element={<Game />} />
                 <Route path="/Winner" element={<Winner />} />
             </>
-        )
+        ), {basename: "/2023-p3a-mpa-react-project-hongolem"}
     )
     return (
         <RouterProvider router={router} />
